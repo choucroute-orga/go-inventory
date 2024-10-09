37ded69ec498cf6185650dd699fc8273d4a5ef41
Arsène Fougerouse <arsene582@gmail.com>
2024-10-09 13:45:42 +0000

    ci: add tests for crud
    Signed-off-by: Arsène Fougerouse <arsene582@gmail.com>

--- api/messages_test.go
+++ api/messages_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 
 	"github.com/sirupsen/logrus"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func doubleMe(x float64) float64 {
@@ -46,9 +45,92 @@ func setupTest(tb testing.TB) (*ApiHandler, func(tb testing.TB)) {
 	}
 }
 
-func TestDoubleMe(t *testing.T) {
+func TestDB(t *testing.T) {
 	teardownSuite := setupSuite(t)
 	defer teardownSuite(t)
+
+	t.Run("Insert ingredient in the DB", func(t *testing.T) {
+		logrus.SetLevel(logrus.DebugLevel)
+		l := logrus.WithField("test", "Insert ingredient in the DB")
+		api, teardownTest := setupTest(t)
+
+		id := "665d9ea9d0ba48dcb11ac3a1"
+
+		i1 := db.Ingredient{
+			ID:           db.NewID(),
+			IngredientID: id,
+			Name:         "Ingredient 1",
+			Quantity:     20,
+			Units:        "g",
+		}
+
+		i2 := db.Ingredient{
+			ID:           db.NewID(),
+			IngredientID: "665d9ea9d0ba48dcb11ac3a1",
+			Name:         "Ingredient 1",
+			Quantity:     1,
+			Units:        "kg",
+		}
+
+		db.InsertOne(l, api.mongo, i1)
+		db.InsertOne(l, api.mongo, i2)
+
+		ingredients, err := db.FindById(l, api.mongo, id)
+
+		// Err should be nil
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+
+		if len(*ingredients) != 2 {
+			t.Errorf("Expected 2 ingredients, got %d", len(*ingredients))
+		}
+
+		// Test the update
+		i1.Quantity = 40
+		db.UpdateOne(l, api.mongo, i1)
+		i, err := db.FindByIdAndUnit(l, api.mongo, id, "g")
+
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+		// Ensure the update was successful
+		if i.Quantity != 40 {
+			t.Errorf("Expected 40, got %f", i.Quantity)
+		}
+		if i.Units != "g" {
+			t.Errorf("Expected g, got %s", i.Units)
+		}
+		if i.IngredientID != id {
+			t.Errorf("Expected %s, got %s", id, i.IngredientID)
+		}
+		if i.Name != "Ingredient 1" {
+			t.Errorf("Expected Ingredient 1, got %s", i.Name)
+		}
+		defer teardownTest(t)
+	})
+
+	t.Run("Insert ingredient and delete in the DB", func(t *testing.T) {
+		logrus.SetLevel(logrus.DebugLevel)
+		l := logrus.WithField("test", "Insert ingredient and delete in the DB")
+		api, teardownTest := setupTest(t)
+		i := db.Ingredient{
+			ID:           db.NewID(),
+			IngredientID: "665d9ea9d0ba48dcb11ac3a1",
+			Name:         "Ingredient 1",
+			Quantity:     20,
+			Units:        "g",
+		}
+		err := db.InsertOne(l, api.mongo, i)
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+		err = db.DeleteOne(l, api.mongo, i.IngredientID)
+		if err != nil {
+			t.Errorf("Expected nil, got %v", err)
+		}
+		defer teardownTest(t)
+	})
 
 	t.Run("Insert one Recipe in the DB", func(t *testing.T) {
 		logrus.SetLevel(logrus.DebugLevel)
@@ -81,32 +163,32 @@ func TestDoubleMe(t *testing.T) {
 			},
 		}
 
-		id1, err := primitive.ObjectIDFromHex("665d9ea9d0ba48dcb11ac3a1")
-		if err != nil {
-			t.Errorf("Error converting hex to ObjectID: %v", err)
-		}
-		id2, _ := primitive.ObjectIDFromHex("665d9ea9d0ba48dcb11ac3a2")
-		id3, _ := primitive.ObjectIDFromHex("665d9ea9d0ba48dcb11ac3a3")
+		id1 := "665d9ea9d0ba48dcb11ac3a1"
+		id2 := "665d9ea9d0ba48dcb11ac3a2"
+		id3 := "665d9ea9d0ba48dcb11ac3a3"
 
 		r1 := db.Ingredient{
-			ID:       id1,
-			Name:     "Ingredient 1",
-			Quantity: 20,
-			Units:    "g",
+			ID:           db.NewID(),
+			IngredientID: id1,
+			Name:         "Ingredient 1",
+			Quantity:     20,
+			Units:        "g",
 		}
 
 		r2 := db.Ingredient{
-			ID:       id2,
-			Name:     "Ingredient 2",
-			Quantity: 20,
-			Units:    "kg",
+			ID:           db.NewID(),
+			IngredientID: id2,
+			Name:         "Ingredient 2",
+			Quantity:     20,
+			Units:        "kg",
 		}
 
 		r3 := db.Ingredient{
-			ID:       id3,
-			Name:     "Ingredient 3",
-			Quantity: 20,
-			Units:    "kg",
+			ID:           db.NewID(),
+			IngredientID: id3,
+			Name:         "Ingredient 3",
+			Quantity:     20,
+			Units:        "kg",
 		}
 
 		db.InsertOne(l, api.mongo, r1)
